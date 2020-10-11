223d8fcf84b0a7a67a579bb922f30e8b6322fc1b
Erkinumur <erkin.umur94@gmail.com>
2020-10-11 21:44:45 +0600

    modify ResumeForm and template resume_create

--- resume/forms.py
+++ resume/forms.py
@@ -1,12 +1,31 @@
-from django.forms import ModelForm, forms
+from django import forms
 
 from .models import Resume
 
 
-class ResumeForm(ModelForm):
+class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
         fields = '__all__'
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={"class": "form-control", "placeholder": 'Имя'}),
+            'last_name': forms.TextInput(
+                attrs={"class": "form-control", "placeholder": 'Фамилия'}),
+            'patronymic': forms.TextInput(
+                attrs={"class": "form-control", "placeholder": 'Отчество'}),
+            'number': forms.NumberInput(
+                attrs={"class": "form-control", "placeholder": 'Номер '
+                                                               'телефона'}),
+            'email': forms.EmailInput(
+                attrs={"class": "form-control", "placeholder": 'Email'}),
+            'department': forms.Select(
+                attrs={"class": "form-control"}),
+            'description': forms.Textarea(
+                attrs={"class": "form-control", "placeholder": 'Описание'}),
+            'experience': forms.Textarea(
+                attrs={"class": "form-control", "placeholder": 'Опыт'}),
+        }
         help_texts = {
             'number': 'Введите номер в формате 996ХХХХХХХХХ'
         }
